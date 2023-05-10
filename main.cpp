159f7e41bdb8df5573817fe35ca5abafcec9fdaf
Dmitriy <golenko_dmitriy@mail.ru>
2023-05-11 05:46:26 +0700

    commit

--- main.cpp
+++ main.cpp
@@ -13,8 +13,8 @@
 #define TURN_OFF 5
 
 #define TASKS_IN_LIST 200
-#define L 100
-#define ITER_MAX 4
+#define L 1000
+#define ITERATION 16
 
 typedef struct Task{
     int repeatNum;
@@ -26,8 +26,9 @@ int iterCounter = 0;
 int curTask = 0;
 int listSize = 0;
 double globalRes = 0;
+double SummaryDisbalance = 0;
 TaskList taskList;
-pthread_mutex_t list_mutex;
+pthread_mutex_t mutex;
 
 void getTaskList() {
     listSize = TASKS_IN_LIST;
@@ -43,10 +44,10 @@ int getTaskFrom(int from) {
     if (flag == FAIL) return FAIL;
     Task recvTask;
     MPI_Recv(&recvTask, 1, MPI_INT, from, ANSWER_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    pthread_mutex_lock(&list_mutex);
+    pthread_mutex_lock(&mutex);
     taskList[listSize] = recvTask;
     listSize++;
-    pthread_mutex_unlock(&list_mutex);
+    pthread_mutex_unlock(&mutex);
     return SUCCESS;
 }
 
@@ -59,20 +60,20 @@ void doTask(Task task) {
 
 void *taskSenderThread(void *args) {
     int flag;
-    while (ITER_MAX > iterCounter) {
+    while (ITERATION > iterCounter) {
         MPI_Status status;
         MPI_Recv(&flag, 1, MPI_INT, MPI_ANY_SOURCE, REQUEST_TAG, MPI_COMM_WORLD,&status);
         if (flag == TURN_OFF) break;
-        pthread_mutex_lock(&list_mutex);
+        pthread_mutex_lock(&mutex);
         if (curTask >= listSize - 1) {
-            pthread_mutex_unlock(&list_mutex);
+            pthread_mutex_unlock(&mutex);
             flag = FAIL;
             MPI_Send(&flag, 1, MPI_INT, status.MPI_SOURCE, ANSWER_TAG, MPI_COMM_WORLD);
             continue;
         }
         listSize--;
         Task sendTask = taskList[listSize];
-        pthread_mutex_unlock(&list_mutex);
+        pthread_mutex_unlock(&mutex);
         flag = SUCCESS;
         MPI_Send(&flag, 1, MPI_INT, status.MPI_SOURCE, ANSWER_TAG, MPI_COMM_WORLD);
         MPI_Send(&sendTask, 1, MPI_INT, status.MPI_SOURCE, ANSWER_TAG, MPI_COMM_WORLD);
@@ -85,29 +86,29 @@ void *taskExecutorThread(void *args) {
     MPI_Barrier(MPI_COMM_WORLD);
     struct timespec start{}, end{};
     iterCounter = 0;
-    while (iterCounter < ITER_MAX) {
+    while (iterCounter < ITERATION) {
         int tasksDone = 0;
         int hasTasks = 1;
-        pthread_mutex_lock(&list_mutex);
+        pthread_mutex_lock(&mutex);
         curTask = 0;
         getTaskList();
-        pthread_mutex_unlock(&list_mutex);
+        pthread_mutex_unlock(&mutex);
         clock_gettime(CLOCK_MONOTONIC, &start);
         while (hasTasks) {
-            pthread_mutex_lock(&list_mutex);
+            pthread_mutex_lock(&mutex);
             if (curTask < listSize) {
                 Task task = taskList[curTask];
-                pthread_mutex_unlock(&list_mutex);
+                pthread_mutex_unlock(&mutex);
                 doTask(task);
                 tasksDone++;
-                pthread_mutex_lock(&list_mutex);
+                pthread_mutex_lock(&mutex);
                 curTask++;
-                pthread_mutex_unlock(&list_mutex);
+                pthread_mutex_unlock(&mutex);
                 continue;
             }
             curTask = 0;
             listSize = 0;
-            pthread_mutex_unlock(&list_mutex);
+            pthread_mutex_unlock(&mutex);
             hasTasks = 0;
             for (int i = 0; i < commSize; i++) {
                 if (i == processRank) continue;
@@ -121,10 +122,9 @@ void *taskExecutorThread(void *args) {
         double minTime, maxTime;
         MPI_Reduce(&timeTaken, &minTime, 1, MPI_DOUBLE, MPI_MIN, 0, MPI_COMM_WORLD);
         MPI_Reduce(&timeTaken, &maxTime, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
-        if (processRank == 0) {
-            printf("Iteration %d, Unbalance time = %lf sec., Unbalance part = %lf.\n", iterCounter, maxTime - minTime, (maxTime - minTime) / maxTime * 100);
-            printf("%d %d\n", iterCounter, ITER_MAX);
-        }
+        pthread_mutex_lock(&mutex);
+        SummaryDisbalance += (maxTime - minTime) / maxTime;
+        pthread_mutex_unlock(&mutex);
         MPI_Barrier(MPI_COMM_WORLD);
         iterCounter++;
     }
@@ -145,7 +145,7 @@ int main(int argc, char **argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &processRank);
     MPI_Comm_size(MPI_COMM_WORLD, &commSize);
     pthread_t threads[2];
-    pthread_mutex_init(&list_mutex, nullptr);
+    pthread_mutex_init(&mutex, nullptr);
     pthread_attr_t attrs;
     if (pthread_attr_init(&attrs) != 0) {
         perror("Cannot initialize attributes");
@@ -175,9 +175,10 @@ int main(int argc, char **argv) {
     if(processRank == 0){
         clock_gettime(CLOCK_MONOTONIC, &endGlobal);
         double timeTaken = endGlobal.tv_sec - startGlobal.tv_sec + 0.000000001 * (double)(endGlobal.tv_nsec - startGlobal.tv_nsec);
-        printf("\n\n\nGlobal time: %lf \n\n\n", timeTaken);
+        printf("\n\n\n Summary disbalance: %lf\n", SummaryDisbalance / (ITERATION) * 100);
+        printf("Global time: %lf", timeTaken);
     }
-    pthread_mutex_destroy(&list_mutex);
+    pthread_mutex_destroy(&mutex);
     MPI_Finalize();
     return 0;
 }
